e4b6683c52b4a31d1646f19dd5dffb4f8dc1d605
TeoB2003 <bolota.teofil2003@gmail.com>
2024-10-18 15:59:43 +0300

    started assignment

---
+++ angular-first-assignment/src/app/photos-area/photos-area.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photos-area',
+  standalone: true,
+  imports: [],
+  templateUrl: './photos-area.component.html',
+  styleUrl: './photos-area.component.css'
+})
+export class PhotosAreaComponent {
+
+}
